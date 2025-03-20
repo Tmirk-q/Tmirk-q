35b5879f36aa942c2bb526893de6d8efa37ecc3c
Tmirk-q <egoraleksandrov162@mail.ru>
2025-03-21 00:28:21 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,130 +1,30 @@
 class ThemeSwitcher {
-     constructor(configuration) {
-          const defaultConfig = {
-               targetElementClass: "application",
-               triggerButtonClass: "theme-switcher-button",
-               triggerButtonActiveClass: "theme-switcher-button-active",
-               darkThemeClass: "dark-theme",
-               themeLocalStorageID: "theme",
-               onChangeTheme: () => {}
-          };
+  constructor() {
+    this.triggerButton = document.querySelector(".theme-switcher-button");
+    this.themeLocalStorageID = "theme";
+    this.darkThemeClass = "dark-theme";
+    this.activeTheme = localStorage.getItem(this.themeLocalStorageID) || "light";
+    this.applyTheme();
+    if (this.triggerButton) {
+      this.triggerButton.addEventListener("click", () => this.toggleTheme());
+    }
+  }
 
-          this.activeTheme = "light";
-          this.options = Object.assign(defaultConfig, configuration);
+  applyTheme() {
+    if (this.activeTheme === "dark") {
+      document.body.classList.add(this.darkThemeClass);
+      if (this.triggerButton) this.triggerButton.textContent = "Тёмная тема";
+    } else {
+      document.body.classList.remove(this.darkThemeClass);
+      if (this.triggerButton) this.triggerButton.textContent = "Светлая тема";
+    }
+  }
 
-          this.initialization();
-     }
-
-     initialization() {
-          this.defineElements();
-          this.applyCurrentTheme();
-          this.switchThemeHandler();
-          this.registerMatchMediaListener();
-     }
-
-     defineElements() {
-          const { triggerButtonClass, targetElementClass } = this.options;
-
-          this.triggerButton = document.querySelector(`.${triggerButtonClass}`);
-          this.targetElement = document.querySelector(`.${targetElementClass}`);
-     }
-
-     onChangeThemeHandler() {
-          const { onChangeTheme } = this.options;
-
-          if (onChangeTheme) {
-               return onChangeTheme(this);
-          }
-     }
-
-     setDarkTheme() {
-          //prettier-ignore
-          const {
-               darkThemeClass,
-               themeLocalStorageID,
-               triggerButtonActiveClass
-          } = this.options;
-
-          localStorage.setItem(themeLocalStorageID, "dark");
-
-          this.activeTheme = "dark";
-          this.targetElement.classList.add(darkThemeClass);
-          this.triggerButton.classList.add(triggerButtonActiveClass);
-
-          this.onChangeThemeHandler();
-     }
-
-     setLightTheme() {
-          //prettier-ignore
-          const {
-               darkThemeClass,
-               themeLocalStorageID,
-               triggerButtonActiveClass
-          } = this.options;
-
-          localStorage.setItem(themeLocalStorageID, "light");
-
-          this.activeTheme = "light";
-          this.targetElement.classList.remove(darkThemeClass);
-          this.triggerButton.classList.remove(triggerButtonActiveClass);
-
-          this.onChangeThemeHandler();
-     }
-
-     setPreferredTheme() {
-          const conditionID1 = window.matchMedia;
-          const conditionID2 = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-          conditionID1 && conditionID2 ? this.setDarkTheme() : this.setLightTheme();
-     }
-
-     applyCurrentTheme() {
-          //prettier-ignore
-          const { themeLocalStorageID } = this.options;
-
-          if (localStorage.getItem(themeLocalStorageID) != null) {
-               this.activeTheme = localStorage.getItem(themeLocalStorageID);
-
-               switch (localStorage.getItem(themeLocalStorageID)) {
-                    case "light":
-                         return this.setLightTheme();
-                    case "dark":
-                         return this.setDarkTheme();
-                    default:
-                         return this.setPreferredTheme();
-               }
-          } else {
-               return this.setPreferredTheme();
-          }
-     }
-
-     switchThemeHandler() {
-          this.triggerButton.addEventListener("click", () => {
-               switch (this.activeTheme) {
-                    case "light":
-                         return this.setDarkTheme();
-                    case "dark":
-                         return this.setLightTheme();
-               }
-          });
-     }
-
-     registerMatchMediaListener() {
-          const prefersColorScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-          prefersColorScheme.addEventListener("change", this.applyCurrentTheme.bind(this));
-     }
+  toggleTheme() {
+    this.activeTheme = this.activeTheme === "light" ? "dark" : "light";
+    localStorage.setItem(this.themeLocalStorageID, this.activeTheme);
+    this.applyTheme();
+  }
 }
 
-new ThemeSwitcher({
-     onChangeTheme: ({ triggerButton, activeTheme }) => {
-          switch (activeTheme) {
-               case "light":
-                    triggerButton.textContent = `Light Theme`;
-                    break;
-               case "dark":
-                    triggerButton.textContent = `Dark Theme`;
-                    break;
-          }
-     }
-});
+new ThemeSwitcher();
